fb8d1110d7ece4280bfa67b12ee34b41a2e6eeee
Thanh Binh <thanhbinhdxdx@gmail.com>
2023-09-23 21:46:40 +0700

    login

---
+++ Controllers/Authentication/FacebookLoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BEARLINGO.Controllers.Authentication
+{
+    public class FacebookLoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/Authentication/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using BEARLINGO.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BEARLINGO.Controllers.Authentication
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            // redict to Views/Authentication/Login.cshtml
+            return View("~/Views/Authentication/Login.cshtml");
+
+        }
+
+    }
+}
