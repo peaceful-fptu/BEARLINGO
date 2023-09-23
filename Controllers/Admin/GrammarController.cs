3464a90ef48ad773dcce43e1f9c5d0d018a27c89
binhp <Binhphhe160537@fpt.edu.vn>
2023-09-24 00:24:38 +0700

    add ngu phap and tu vung

---
+++ Controllers/Admin/GrammarController.cs
@@ -0,0 +1,236 @@
+﻿using BEARLINGO.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BEARLINGO.Controllers.Admin
+{
+    public class GrammarController : Controller
+    {
+        private readonly BearlingoContext _context;
+
+        public GrammarController()
+        {
+            _context = new BearlingoContext();
+        }
+
+        public IActionResult Topic()
+        {
+            var listChuDe = this.getChuDe();
+            ViewData["listChuDe"] = listChuDe;
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult TopicDetail(int idNguPhap)
+        {
+            var listNguPhap = this.getNguPhap(idNguPhap);
+            ViewData["listNguPhap"] = listNguPhap;
+            return View();
+        }
+
+        public List<ChuDeNguPhap> getChuDe()
+        {
+            var result = from chude in this._context.ChuDeNguPhaps
+                         join qtv in this._context.Qtvs
+                            on chude.Idqtv equals qtv.Idqtv
+                         select new ChuDeNguPhap
+                         {
+                             IdchuDeNguPhap = chude.IdchuDeNguPhap,
+                             TenNguPhap = chude.TenNguPhap,
+                             Stt = chude.Stt,
+                             Idqtv = qtv.Idqtv,
+                         };
+
+            if (result != null)
+            {
+                return result.ToList();
+            }
+
+            return new List<ChuDeNguPhap>();
+        }
+
+        public List<NguPhap> getNguPhap(int id)
+        {
+            var result = from nguphap in this._context.NguPhaps
+                         join chude in this._context.ChuDeNguPhaps
+                            on nguphap.IdchuDeNguPhap equals chude.IdchuDeNguPhap
+                         where nguphap.IdchuDeNguPhap == id
+                         select new NguPhap
+                         {
+                             IdnguPhap = nguphap.IdnguPhap,
+                             TieuDe = nguphap.TieuDe,
+                             CachDung = nguphap.CachDung,
+                             CauTruc = nguphap.CauTruc,
+                             ViDu = nguphap.ViDu,
+                             BoSung = nguphap.BoSung,
+                             LuuY = nguphap.LuuY,
+                             IdchuDeNguPhap = chude.IdchuDeNguPhap,
+                         };
+
+            if (result != null)
+            {
+                return result.ToList();
+            }
+
+            return new List<NguPhap>();
+        }
+
+        [HttpPost]
+        public IActionResult AddChuDe(string tenNguPhap, int stt, int idQtv)
+        {
+            try
+            {
+                ChuDeNguPhap chuDeNguPhap = new ChuDeNguPhap
+                {
+                    TenNguPhap = tenNguPhap,
+                    Stt = stt,
+                    Idqtv = idQtv,
+                };
+
+                this._context.ChuDeNguPhaps.Add(chuDeNguPhap);
+                this._context.SaveChanges();
+                return RedirectToAction("Topic", "Grammar");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddNguPhap(string tieuDe, string cachDung, string cauTruc, string viDu, string boSung, string luuY, int idChuDe)
+        {
+            try
+            {
+                NguPhap nguPhap = new NguPhap
+                {
+                    TieuDe = tieuDe,
+                    CachDung = cachDung,
+                    CauTruc = cauTruc,
+                    ViDu = viDu,
+                    BoSung = boSung,
+                    LuuY = luuY,
+                    IdchuDeNguPhap = idChuDe,
+                };
+
+                this._context.NguPhaps.Add(nguPhap);
+                this._context.SaveChanges();
+
+                return RedirectToAction("TopicDetail", "Grammar", new { idNguPhap = idChuDe });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DeleteChuDe(int idChuDe)
+        {
+            var chuDe = this._context.ChuDeNguPhaps.FirstOrDefault(item => item.IdchuDeNguPhap == idChuDe);
+            var nguPhap = this._context.NguPhaps.Where(item => item.IdchuDeNguPhap == idChuDe).ToList();
+            using (var transaction = _context.Database.BeginTransaction())
+            {
+                try
+                {
+                    if (nguPhap != null)
+                    {
+                        this._context.NguPhaps.RemoveRange(nguPhap);
+                        this._context.SaveChanges();
+                    }
+                    if (chuDe != null)
+                    {
+                        this._context.ChuDeNguPhaps.Remove(chuDe);
+                        this._context.SaveChanges();
+                    }
+
+                    transaction.Commit();
+
+                    return RedirectToAction("Topic", "Grammar");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult DeleteNguPhap(int idNguPhap)
+        {
+            var nguPhap = this._context.NguPhaps.FirstOrDefault(item => item.IdnguPhap == idNguPhap);
+            try
+            {
+                if (nguPhap != null)
+                {
+                    this._context.Remove(nguPhap);
+                    this._context.SaveChanges();
+                }
+
+                return RedirectToAction("TopicDetail", "Grammar", new { idNguPhap = idNguPhap });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateChuDe(int idChuDe, string tenNguPhap, int stt, int idQtv)
+        {
+            var chuDe = this._context.ChuDeNguPhaps.FirstOrDefault(item => item.IdchuDeNguPhap == idChuDe);
+            try
+            {
+                if (chuDe != null)
+                {
+                    chuDe.TenNguPhap = tenNguPhap;
+                    chuDe.Stt = stt;
+                    chuDe.Idqtv = idQtv;
+                }
+
+                return RedirectToAction("Topic", "Grammar");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateNguPhap(int idNguPhap, string tieuDe, string cachDung, string cauTruc, string viDu, string boSung, string luuY, int idChuDe)
+        {
+            var nguPhap = this._context.NguPhaps.FirstOrDefault(item => item.IdnguPhap == idNguPhap);
+            try
+            {
+                if (nguPhap != null)
+                {
+                    nguPhap.TieuDe = tieuDe;
+                    nguPhap.CachDung = cachDung;
+                    nguPhap.CauTruc = cauTruc;
+                    nguPhap.ViDu = viDu;
+                    nguPhap.BoSung = boSung;
+                    nguPhap.LuuY = luuY;
+                    nguPhap.IdchuDeNguPhap = idChuDe;
+                }
+
+                return RedirectToAction("TopicDetail", "Grammar", new { idNguPhap = idChuDe });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/Admin/VocabularyController.cs
@@ -0,0 +1,237 @@
+﻿using BEARLINGO.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BEARLINGO.Controllers.Admin
+{
+    public class VocabularyController : Controller
+    {
+        private readonly BearlingoContext _context;
+
+        public VocabularyController()
+        {
+            _context = new BearlingoContext();
+        }
+
+        public IActionResult Vocabulary()
+        {
+            var listChuDe = this.getChuDe();
+            ViewData["listChuDe"] = listChuDe;
+            return View();
+        }
+
+        public IActionResult VocabularyDetail(int idChuDe)
+        {
+            var listTuVung = this.getTuVung(idChuDe);
+            ViewData["listTuVung"] = listTuVung;
+            return View();
+        }
+
+        public List<ChuDeTuVung> getChuDe()
+        {
+            var result = from chude in this._context.ChuDeTuVungs
+                         join qtv in this._context.Qtvs
+                            on chude.Idqtv equals qtv.Idqtv
+                         select new ChuDeTuVung
+                         {
+                             IdchuDeTuVung = chude.IdchuDeTuVung,
+                             ChuDe = chude.ChuDe,
+                             Stt = chude.Stt,
+                             Idqtv = qtv.Idqtv,
+                         };
+
+            if (result != null)
+            {
+                return result.ToList();
+            }
+
+            return new List<ChuDeTuVung>();
+        }
+
+        public List<TuVung> getTuVung(int idChuDe)
+        {
+            var result = from tuvung in this._context.TuVungs
+                         join chude in this._context.ChuDeTuVungs
+                            on tuvung.IdchuDeTuVung equals chude.IdchuDeTuVung
+                         where tuvung.IdchuDeTuVung == idChuDe
+                         select new TuVung
+                         {
+                             IdtuVung = tuvung.IdtuVung,
+                             TuVung1 = tuvung.TuVung1,
+                             PhatAm = tuvung.PhatAm,
+                             LoaiTu = tuvung.LoaiTu,
+                             NghiaTuVung = tuvung.NghiaTuVung,
+                             ViDuTuVung = tuvung.ViDuTuVung,
+                             IdchuDeTuVung = tuvung.IdchuDeTuVung,
+                         };
+
+            if (result != null)
+            {
+                return result.ToList();
+            }
+
+            return new List<TuVung>();
+        }
+
+        [HttpPost]
+        public IActionResult AddChuDe(string chuDe, int stt, int idQtv)
+        {
+            try
+            {
+                ChuDeTuVung chuDeTuVung = new ChuDeTuVung
+                {
+                    ChuDe = chuDe,
+                    Stt = stt,
+                    Idqtv = idQtv,
+                };
+
+                this._context.ChuDeTuVungs.Add(chuDeTuVung);
+                this._context.SaveChanges();
+
+                return RedirectToAction("Vocabulary", "Vocabulary");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTuVung(string tuVung1, string phatAm, string loaiTu, string nghiaTuVung, string viDuTuVung, int idChuDeTuVung)
+        {
+            try
+            {
+                TuVung tuVung = new TuVung
+                {
+                    TuVung1 = tuVung1,
+                    PhatAm = phatAm,
+                    LoaiTu = loaiTu,
+                    NghiaTuVung = nghiaTuVung,
+                    ViDuTuVung = viDuTuVung,
+                    IdchuDeTuVung = idChuDeTuVung,
+                };
+
+                this._context.TuVungs.Add(tuVung);
+                this._context.SaveChanges();
+
+                return RedirectToAction("VocabularyDetail", "Vocabulary", new { idChuDe = idChuDeTuVung });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult DeleteChuDeTuVung(int idChuDeTuVung)
+        {
+            var chuDeTuVung = this._context.ChuDeTuVungs.FirstOrDefault(item => item.IdchuDeTuVung == idChuDeTuVung);
+            var tuVung = this._context.TuVungs.Where(item => item.IdchuDeTuVung == idChuDeTuVung);
+
+            using (var transaction = this._context.Database.BeginTransaction())
+            {
+                try
+                {
+                    if (tuVung != null)
+                    {
+                        foreach (var item in tuVung)
+                        {
+                            this._context.TuVungs.Remove(item);
+                            this._context.SaveChanges();
+                        }
+                    }
+
+                    if (chuDeTuVung != null)
+                    {
+                        this._context.ChuDeTuVungs.Remove(chuDeTuVung);
+                        this._context.SaveChanges();
+                    }
+
+                    transaction.Commit();
+
+                    return RedirectToAction("Vocabulary", "Vocabulary");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult DeleteTuVung(int idTuVung, int idChuDeTuVung)
+        {
+            var tuVung = this._context.TuVungs.FirstOrDefault(item => item.IdtuVung == idTuVung);
+            try
+            {
+                if (tuVung != null)
+                {
+                    this._context.Remove(tuVung);
+                    this._context.SaveChanges();
+                }
+
+                return RedirectToAction("VocabularyDetail", "Vocabulary", new { idChuDe = idChuDeTuVung });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateChuDeTuVung(int idChuDeTuVung, string chuDe, int stt, int idQtv)
+        {
+            var chuDeTuVung = this._context.ChuDeTuVungs.FirstOrDefault(item => item.IdchuDeTuVung == idChuDeTuVung);
+            try
+            {
+                if (chuDeTuVung != null)
+                {
+                    chuDeTuVung.ChuDe = chuDe;
+                    chuDeTuVung.Stt = stt;
+                    chuDeTuVung.Idqtv = idQtv;
+                }
+
+                return RedirectToAction("Vocabulary", "Vocabulary");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult UpdateTuVung(int idTuVung, string tuVung1, string phatAm, string loaiTu, string nghiaTuVung, string viDuTuVung, int idChuDeTuVung)
+        {
+            var tuVung = this._context.TuVungs.FirstOrDefault(item => item.IdtuVung == idTuVung);
+            try
+            {
+                if (tuVung != null)
+                {
+                    tuVung.TuVung1 = tuVung1;
+                    tuVung.PhatAm = phatAm;
+                    tuVung.LoaiTu = loaiTu;
+                    tuVung.NghiaTuVung = nghiaTuVung;
+                    tuVung.ViDuTuVung = viDuTuVung;
+                    tuVung.IdchuDeTuVung = idChuDeTuVung;
+                }
+
+                return RedirectToAction("VocabularyDetail", "Vocabulary", new { idChuDe = idChuDeTuVung });
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return View();
+        }
+    }
+}
