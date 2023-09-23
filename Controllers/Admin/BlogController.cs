4c4f0d817e5bc9d32ff3e1614501eb5bb4a4c989
hieu0317 <vuminhhieu171103@gmail.com>
2023-09-24 01:10:43 +0700

    commit admin blog and tip

--- Controllers/Admin/BlogController.cs
+++ Controllers/Admin/BlogController.cs
@@ -1,12 +1,55 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BEARLINGO.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BEARLINGO.Controllers.Admin
 {
     public class BlogController : Controller
     {
-        public IActionResult Blog()
+        public IActionResult BlogManage()
         {
+            using(BearlingoContext ctx = new BearlingoContext())
+            {
+                var blogs = ctx.Blogs.ToList();
+                ViewBag.blogs = blogs;
+            }
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult addBlog(Blog blog)
+        {
+            using (BearlingoContext ctx = new BearlingoContext())
+            {
+                int IDQTV = (int)HttpContext.Session.GetInt32("IDQTV");
+                blog.Idqtv = IDQTV;
+                ctx.Blogs.Add(blog);
+                ctx.SaveChanges();  
+            }
+            return View("Blog");
+        }
+
+        public IActionResult deleteBlog(int id)
+        {
+            using (BearlingoContext ctx = new BearlingoContext())
+            {
+                Blog blog = ctx.Blogs.FirstOrDefault(bl => bl.Idblog ==  id);
+                ctx.Blogs.Remove(blog);
+                ctx.SaveChanges();
+            }
+            return View("Blog");
+        }
+
+        [HttpPost]
+        public IActionResult editBlog(Blog newBlog)
+        {
+            using (BearlingoContext ctx = new BearlingoContext())
+            {
+                Blog oldBlog = ctx.Blogs.FirstOrDefault(bl => bl.Idblog == newBlog.Idblog);
+                ctx.Blogs.Remove(oldBlog);
+                ctx.Blogs.Add(newBlog);
+                ctx.SaveChanges();                  
+            }
+            return View("Blog");
         }
     }
 }
---
+++ Controllers/Admin/TipController.cs
@@ -0,0 +1,53 @@
+﻿using BEARLINGO.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BEARLINGO.Controllers.Admin
+{
+    public class TipController : Controller
+    {
+        public IActionResult tipManage()
+        {
+            using(BearlingoContext ctx = new BearlingoContext())
+            {
+                var tips = ctx.Tips.ToList();
+                ViewBag.tips = tips;
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult addTip(Tip tip)
+        {
+            using (BearlingoContext ctx = new BearlingoContext())
+            {
+                ctx.Tips.Add(tip);
+                ctx.SaveChanges();
+            }
+            return RedirectToAction("tipManage");
+        }
+
+        [HttpPost]
+        public IActionResult editTip(Tip newTip)
+        {
+            using (BearlingoContext ctx = new BearlingoContext())
+            {
+                Tip oldTip = ctx.Tips.FirstOrDefault(t => t.Idtips == newTip.Idtips);
+                ctx.Tips.Remove(oldTip);
+                ctx.Tips.Add(newTip);
+                ctx.SaveChanges();
+            }
+            return View();
+        }
+
+        public IActionResult deleteTip(int id)
+        {
+            using (BearlingoContext ctx = new BearlingoContext())
+            {
+                Tip tip = ctx.Tips.FirstOrDefault(t => t.Idtips == id);
+                ctx.Tips.Remove(tip);
+                ctx.SaveChanges();  
+            }
+            return RedirectToAction("tipManage");
+        }
+    }
+}
