38cd22ebb9499827603130f5c598d2845f0ca569
Thanh Binh <thanhbinhdxdx@gmail.com>
2023-09-28 10:35:47 +0700

    fix register

--- Controllers/Authentication/RegisterController.cs
+++ Controllers/Authentication/RegisterController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BEARLINGO.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BEARLINGO.Controllers.Authentication
 {
@@ -6,7 +7,41 @@ namespace BEARLINGO.Controllers.Authentication
     {
         public IActionResult Index()
         {
-            return View();
+            return View("~/Views/Authentication/Register.cshtml");
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Register(NguoiDung nguoiDung)
+        {
+            using (var db = new BearlingoContext())
+            {
+                if (ModelState.IsValid)
+                {
+                    // check gmail exist
+                    var user = db.NguoiDungs.SingleOrDefault(x => x.Gmail == nguoiDung.Gmail);
+                    if (user != null)
+                    {
+                        ViewBag.messageErrorEmail = "Email đã tồn tại!";
+                        return View("Index");
+                    }
+                    // check username exist
+                    var user2 = db.NguoiDungs.SingleOrDefault(x => x.TenDangNhap == nguoiDung.TenDangNhap);
+                    if (user2 != null)
+                    {
+                        ViewBag.messageErrorUsername = "Tên đăng nhập đã tồn tại!";
+                        return View("Index");
+                    }
+                    nguoiDung.IDDangNhap = 0;
+                    db.NguoiDungs.Add(nguoiDung);
+                    db.SaveChanges();
+                    return RedirectToAction("Index", "Login");
+                }
+                else
+                {
+                    return RedirectToAction("Index", "Register");
+                }
+            }
+        }
+
     }
 }
