cc68ddc0dc6898273e3a9d1b69e1a222c5526286
Thanh Binh <thanhbinhdxdx@gmail.com>
2023-10-04 01:35:25 +0700

    sr baby

--- Controllers/Admin/GrammarController.cs
+++ Controllers/Admin/GrammarController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Storage;
+using static BEARLINGO.Program;
 
 namespace BEARLINGO.Controllers.Admin
 {
